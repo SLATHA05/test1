0fb89e5f334bc3680937d71e6bfbfb85898d67af
S.LATHA <lathas@rvsgroup.com>
2026-07-30 15:10:32 +0530

    Create college.sql

---
+++ college.sql
@@ -0,0 +1,11 @@
+-- ===========================================
+-- Assignment: Create CollegeDB and Department Table
+-- Name:
+-- Register Number:
+-- ===========================================
+
+-- Note:
+-- SQLite does not support CREATE DATABASE.
+-- Assume the database file is CollegeDB.db.
+
+-- Write your SQL statements below.
